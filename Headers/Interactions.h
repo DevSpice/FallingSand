7f049ecd5ce83b6ca6e25d3ea7728d1e1e4f1040
Trent Houle <trenthoule@Trents-MacBook-Air-3.local>
2026-03-10 16:18:53 -0500

    Adding interactions

---
+++ Headers/Interactions.h
@@ -0,0 +1,30 @@
+#ifndef INTERACTIONS_H
+# define INTERACTIONS_H
+
+#include <unordered_map>
+#include <vector>
+#include <memory>
+
+
+enum class Element {
+    // enums representing each element, because I couldn't find a way to actully store
+    // types in something.
+    STEAM,
+    WATER,
+    SAND,
+    FIRE,
+    NONE
+};
+
+enum class Interaction {
+    // Defines the interaction which this element has upon being near another element
+    
+    // E.g., for water, its FIRE interaction is REPLACE with STEAM, because the water
+    // itself will handle that. But for fire, its WATER interaction is DESTROY, since
+    // the water extinguishes the fire, so the fire itself will handle that.
+    REPLACE,
+    DESTROY
+};
+
+
+
